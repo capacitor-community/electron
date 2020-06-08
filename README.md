8de11da33dbf99846ff3146838fafda8a37d9586
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2020-06-07 20:27:38 -0400

    feat: turn electron platform into community pllugin

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Capacitor community support for the Electron platform.
 
 | Maintainer | GitHub | Social | Sponsoring Company | Primary  |
 | -----------| -------| -------| ------------------ | -------- |
-| Mike S. | [mlynch](https://github.com/Mike-Summ) | [@IT_MikeS](https://twitter.com/IT_MikeS) | Volunteer | Yes |
+| Mike S. | [IT-MikeS](https://github.com/IT-MikeS) | [@IT_MikeS](https://twitter.com/IT_MikeS) | Volunteer | Yes |
 | Max Lynch | [mlynch](https://github.com/mlynch) | [@maxlynch](https://twitter.com/maxlynch) | Ionic | *No* |
 
 Maintenance Status: Actively Maintained
@@ -22,3 +22,19 @@ Maintenance Status: Actively Maintained
 ## Usage
 
 More info soon
+
+## Configuration for Popular Frameworks/Libraries
+
+- __Ionic/React__
+  1. Add `homepage: "./",` to your apps `package.json`
+  2. In `YOUR_APP_ROOT/public/index.html` change the `<base href="/">` to `<base href="./">`
+
+- __Ionic/Angular__
+  1. Modify your scripts section entry of `"build": "ng build"` in your apps `package.json` to read as `"build": "ng build --base-href ./"` 
+
+## Using your own image for the Splash Screen
+
+`@capacitor-community/electron` looks in the `splash_assets` folder of the `YOUR_APP_ROOT/electron` folder for a `splash.png` file by default, you can use your own image by using one of the folloing methods:
+
+1. Editing the `splash.png` file directly.
+2. Place your own image file into the `splash_assets` folder and pass the `imageFileName` property as part of `splashOptions` into `splashScreen = new CapacitorSplashScreen(mainWindow);`. For example if your image was named `myImage.png` your would pass it like: `splashScreen = new CapacitorSplashScreen(mainWindow, {imageFileName: 'myImage.png'});`
---
+++ electron_template/index.js
@@ -0,0 +1,118 @@
+const { app, BrowserWindow, Menu, dialog } = require('electron');
+const isDevMode = require('electron-is-dev');
+const path = require('path');
+const { CapacitorSplashScreen, CapacitorDeeplinking, configCapacitor } = require('@capacitor-community/electron');
+
+// Place holders for our windows so they don't get garbage collected.
+let mainWindow = null;
+
+// Placeholder for SplashScreen ref
+let splashScreen = null;
+
+// Placeholder for deeplinking
+let deepLinking = null;
+// This is the function that will run when a deeplink is executed.
+const deepLinkingHandler = (deeplinkingUrl) => {
+  //Do something with passed deeplinking url (ex: mycapacitorapp://testing)
+  console.log(deeplinkingUrl);
+  dialog.showMessageBox(mainWindow, {message: deeplinkingUrl, title: 'Log', buttons: ['Okay']});
+};
+
+//Change this if you do not wish to have a splash screen
+let useSplashScreen = true;
+
+// Create simple menu for easy devtools access, and for demo
+const menuTemplateDev = [
+  {
+    label: 'Options',
+    submenu: [
+      {
+        label: 'Open Dev Tools',
+        click() {
+          mainWindow.openDevTools();
+        },
+      },
+    ],
+  },
+];
+
+async function createWindow () {
+  // Define our main window size
+  mainWindow = new BrowserWindow({
+    height: 920,
+    width: 1600,
+    show: false,
+    icon: path.join(app.getAppPath(), 'appIcon.png'),
+    title: 'Capacitor App',
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+      // Use preload to inject the electron varriant overrides for capacitor plugins.
+      // Note: any windows you spawn that you want to include capacitor plugins must have this preload.
+      preload: path.join(__dirname, 'node_modules', '@capacitor-community', 'electron', 'dist', 'electron-bridge.js')
+    }
+  });
+
+  // Initialize Deeplinking for given custom protocol.
+  deepLinking = new CapacitorDeeplinking(mainWindow, {customProtocol: "mycapacitorapp"});
+
+  // Call to configure the useragent for capacitor.
+  // Note: any windows you spawn that you want to include capacitor plugins must have this config function applied.
+  configCapacitor(mainWindow);
+
+  // Check if electron is in dev mode.
+  if (isDevMode) {
+    // Set our above template to the Menu Object if we are in development mode, dont want users having the devtools.
+    Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplateDev));
+    // If we are developers we might as well open the devtools by default.
+    mainWindow.webContents.openDevTools();
+  }
+
+  // This function will get called after the SplashScreen timeout and load your content into the main window.
+  const loadMainWindow = () => {
+    // Setup the handler for deeplinking if it has been setup.
+    if(deepLinking !== null)
+      deeplinking.init(deepLinkingHandler);
+
+    // Here we use a file referance but you could also reference a dev server instead:
+    // mainWindow.loadURL(`http://localhost:3000`);
+    mainWindow.loadFile(`./app/index.html`);
+
+    // After your content is loaded in we show the user the window. 
+    mainWindow.webContents.on('dom-ready', () => {
+      mainWindow.show();
+    });
+  }
+
+  //Based on Splashscreen choice actually load the window.
+  if(useSplashScreen) {
+    splashScreen = new CapacitorSplashScreen(mainWindow);
+    splashScreen.init(loadMainWindow);
+  } else {
+    loadMainWindow();
+  }
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some Electron APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+// Define any IPC or other custom functionality below here
