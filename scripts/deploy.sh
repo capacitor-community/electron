0ee4c724a2376e5a04f7d8a135685d2f365ca02a
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2020-06-12 14:01:50 -0400

    chore: lerna integration

---
+++ scripts/deploy.sh
@@ -0,0 +1,14 @@
+set -e
+
+LERNA_JSON=`cat lerna.json`;
+export LERNA_VERSION="$(node -pe "JSON.parse(\`$LERNA_JSON\`)['version']")"
+
+echo "Deploying Capacitor v$LERNA_VERSION"
+
+# This is what we do instead of letting lerna git commit for us
+git add lerna.json
+git add electron-core/package.json
+git add electron-platform/package.json
+git commit -m "Release v$LERNA_VERSION"
+git tag $LERNA_VERSION -m $LERNA_VERSION
+git push --follow-tags origin master
