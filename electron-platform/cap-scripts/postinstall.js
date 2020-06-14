2f4f6e985edfe111edc35b4acebffc906219651d
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2020-06-14 11:50:00 -0400

    feat: spinners in postinstall script

--- electron-platform/cap-scripts/postinstall.js
+++ electron-platform/cap-scripts/postinstall.js
@@ -16,51 +16,88 @@ function runExec(command) {
 }
 
 async function doPostInstall() {
-  if (!fs.existsSync(path.join(__dirname, "../", ".no-postinstall-script"))) {
-    const usersProjectCapConfig = path.join(
-      process.env.INIT_CWD,
-      "capacitor.config.json"
-    );
-    const srcDir = path.join(__dirname, "../", "electron_template");
-    const destDir = path.join(process.env.INIT_CWD, "electron");
-    if (usersProjectCapConfig) {
-      const capConfigJson = JSON.parse(
-        fs.readFileSync(usersProjectCapConfig, "utf-8")
+  const ora = require("ora");
+  const platformSpinner = ora("Installing Electron Platform...").start();
+  const requirementsStepSpinner = ora({
+    text: "Checking requirements...",
+    indent: 4,
+  });
+  const createElectronFolderStepSpinner = ora({
+    text: "Checking requirements...",
+    indent: 4,
+  });
+  const copyCapacitorConfigStepSpinner = ora({
+    text: "Checking requirements...",
+    indent: 4,
+  });
+  const copyWebDirStepSpinner = ora({
+    text: "Checking requirements...",
+    indent: 4,
+  });
+  const npmInstallForPlatformStepSpinner = ora({
+    text: "Checking requirements...",
+    indent: 4,
+  });
+  try {
+    requirementsStepSpinner.start();
+    if (!fs.existsSync(path.join(__dirname, "../", ".no-postinstall-script"))) {
+      const usersProjectCapConfig = path.join(
+        process.env.INIT_CWD,
+        "capacitor.config.json"
       );
-      if (capConfigJson.webDir) {
-        const webDirPath = path.join(
-          process.env.INIT_CWD,
-          capConfigJson.webDir
+      const srcDir = path.join(__dirname, "../", "electron_template");
+      const destDir = path.join(process.env.INIT_CWD, "electron");
+      if (usersProjectCapConfig) {
+        const capConfigJson = JSON.parse(
+          fs.readFileSync(usersProjectCapConfig, "utf-8")
         );
-        if (fs.exists(webDirPath)) {
-          if (!fs.exists(destDir)) {
-            fs.mkdirSync(destDir, { recursive: true });
-            fse.copySync(srcDir, destDir);
-            fse.copySync(
-              usersProjectCapConfig,
-              path.join(destDir, "capacitor.config.json")
-            );
-            fse.copySync(webDirPath, path.join(destDir, "app"));
-            console.log(await runExec(`cd ${destDir} && npm i`));
-            console.log("Electron platform added!");
+        if (capConfigJson.webDir) {
+          const webDirPath = path.join(
+            process.env.INIT_CWD,
+            capConfigJson.webDir
+          );
+          if (fs.exists(webDirPath)) {
+            if (!fs.exists(destDir)) {
+              requirementsStepSpinner.succeed();
+              createElectronFolderStepSpinner.start();
+              fs.mkdirSync(destDir, { recursive: true });
+              fse.copySync(srcDir, destDir);
+              createElectronFolderStepSpinner.succeed();
+              copyCapacitorConfigStepSpinner.start();
+              fse.copySync(
+                usersProjectCapConfig,
+                path.join(destDir, "capacitor.config.json")
+              );
+              copyCapacitorConfigStepSpinner.succeed();
+              copyWebDirStepSpinner.start();
+              fse.copySync(webDirPath, path.join(destDir, "app"));
+              copyWebDirStepSpinner.succeed();
+              npmInstallForPlatformStepSpinner.start();
+              console.log(await runExec(`cd ${destDir} && npm i`));
+              npmInstallForPlatformStepSpinner.succeed();
+              platformSpinner.succeed("Electron platform added!");
+            } else {
+              throw new Error("Electron platform folder already exists.");
+            }
           } else {
-            throw new Error("Electron platform folder already exists.");
+            throw new Error(
+              "The webDir referenced in capcacitor.config.json does not exsist."
+            );
           }
         } else {
           throw new Error(
-            "The webDir referenced in capcacitor.config.json does not exsist."
+            "Property webDir is not defined in capacitor.config.json."
           );
         }
       } else {
         throw new Error(
-          "Property webDir is not defined in capacitor.config.json."
+          "No capacitor.config.json file found. Did you initiate capcacitor for this project?"
         );
       }
-    } else {
-      throw new Error(
-        "No capacitor.config.json file found. Did you initiate capcacitor for this project?"
-      );
     }
+  } catch (e) {
+    requirementsStepSpinner.fail(`${e.message}`);
+    throw e;
   }
 }
 
