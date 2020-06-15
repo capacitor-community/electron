7b691b7acf25f64acf6b0bb5dfc58826cf51eeea
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2020-06-15 09:25:46 -0400

    chore(platform): cleanup postinstall & copy script

--- electron-platform/cap-scripts/copy.js
+++ electron-platform/cap-scripts/copy.js
@@ -1,30 +1,78 @@
 const fs = require("fs");
 const path = require("path");
 const fse = require("fs-extra");
+const ora = require("ora");
 
-async function doCapCopy() {
-  const capConfigJson = JSON.parse(
-    fs.readFileSync(usersProjectCapConfig, "utf-8")
+function checkRequirements() {
+  const outPaths = {
+    errorText: null,
+    usersProjectCapConfigPath: null,
+    destTemplatePath: null,
+    webAppPath: null,
+  };
+  const usersProjectCapConfig = path.join(
+    process.env.INIT_CWD,
+    "capacitor.config.json"
   );
-  if (capConfigJson.webDir) {
-    const webDirPath = path.join(process.env.INIT_CWD, capConfigJson.webDir);
-    const destPath = path.join(process.env.INIT_CWD, "electron");
-    if (fs.existsSync(webDirPath)) {
-      if (fs.existsSync(destPath)) {
-        fse.copySync(webDirPath, path.join(destPath, "app"));
-        console.log("Webapp copied to Electron Platform!");
+  const destDir = path.join(process.env.INIT_CWD, "electron");
+  if (usersProjectCapConfig) {
+    const capConfigJson = JSON.parse(
+      fs.readFileSync(usersProjectCapConfig, "utf-8")
+    );
+    if (capConfigJson.webDir) {
+      const webDirPath = path.join(process.env.INIT_CWD, capConfigJson.webDir);
+      if (fs.existsSync(webDirPath)) {
+        if (fs.existsSync(destDir)) {
+          outPaths.destTemplatePath = destDir;
+          outPaths.usersProjectCapConfigPath = usersProjectCapConfig;
+          outPaths.webAppPath = webDirPath;
+        } else {
+          outPaths.errorText = "Electron platform not installed.";
+        }
       } else {
-        throw new Error(
-          "Electron platform not found in project. Add with: npm i @capacitor-community/electron-platform"
-        );
+        outPaths.errorText =
+          "WebDir defined in capacitor.config.json does not exist, did you build your webapp?";
       }
     } else {
-      throw new Error(
-        "The webDir referenced in capcacitor.config.json does not exist."
-      );
+      outPaths.errorText = "No webDir defined in capacitor.config.json.";
     }
   } else {
-    throw new Error("webDir is not defined in capacitor.config.json");
+    outPaths.errorText =
+      "capacitor.config.json does not exist, did you setup capacitor in your project root?";
+  }
+  return outPaths;
+}
+
+function copyBuiltWebApp(webDirPath, destDir) {
+  const copyWebDirStepSpinner = ora({
+    text: "[Electron Platform - Copy] Copying webDir to platform...",
+  });
+  copyWebDirStepSpinner.start();
+  try {
+    const platformAppFolder = path.join(destDir, "app");
+    if (fs.existsSync(platformAppFolder)) fse.removeSync(platformAppFolder);
+    fse.copySync(webDirPath, platformAppFolder);
+    copyWebDirStepSpinner.succeed();
+  } catch (e) {
+    copyWebDirStepSpinner.fail();
+    throw e;
+  }
+}
+
+async function doCapCopy() {
+  const requirementsStepSpinner = ora({
+    text: "[Electron Platform - Copy] Checking requirements...",
+  });
+  requirementsStepSpinner.start();
+  const paths = checkRequirements();
+  if (paths.errorText === null) {
+    copyBuiltWebApp(paths.webAppPath, paths.destTemplatePath);
+    console.log(
+      "[Electron Platform - Copy] WebApp copied to electron platform!"
+    );
+  } else {
+    requirementsStepSpinner.fail();
+    throw new Error(errorText);
   }
 }
 
--- electron-platform/cap-scripts/postinstall.js
+++ electron-platform/cap-scripts/postinstall.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const fse = require("fs-extra");
 const { exec } = require("child_process");
+const ora = require("ora");
 
 function runExec(command) {
   return new Promise((resolve, reject) => {
@@ -15,87 +16,134 @@ function runExec(command) {
   });
 }
 
-async function doPostInstall() {
-  const ora = require("ora");
-  const platformSpinner = ora("Installing Electron Platform...").start();
-  const requirementsStepSpinner = ora({
-    text: "Checking requirements...",
-  });
-  const createElectronFolderStepSpinner = ora({
-    text: "Creating electron platform folder...",
-  });
-  const copyCapacitorConfigStepSpinner = ora({
-    text: "Copying capacitor.config.json to platform...",
-  });
-  const copyWebDirStepSpinner = ora({
-    text: "Copying webDir to platform...",
-  });
-  const npmInstallForPlatformStepSpinner = ora({
-    text: "Running npm install on platform...",
-  });
-  try {
-    requirementsStepSpinner.start();
-    if (!fs.existsSync(path.join(__dirname, "../", ".no-postinstall-script"))) {
-      const usersProjectCapConfig = path.join(
-        process.env.INIT_CWD,
-        "capacitor.config.json"
-      );
-      const srcDir = path.join(__dirname, "../", "electron_template");
-      const destDir = path.join(process.env.INIT_CWD, "electron");
-      if (usersProjectCapConfig) {
-        const capConfigJson = JSON.parse(
-          fs.readFileSync(usersProjectCapConfig, "utf-8")
-        );
-        if (capConfigJson.webDir) {
-          const webDirPath = path.join(
-            process.env.INIT_CWD,
-            capConfigJson.webDir
-          );
-          if (fs.existsSync(webDirPath)) {
-            if (!fs.existsSync(destDir)) {
-              requirementsStepSpinner.succeed();
-              createElectronFolderStepSpinner.start();
-              fs.mkdirSync(destDir, { recursive: true });
-              fse.copySync(srcDir, destDir);
-              createElectronFolderStepSpinner.succeed();
-              copyCapacitorConfigStepSpinner.start();
-              fse.copySync(
-                usersProjectCapConfig,
-                path.join(destDir, "capacitor.config.json")
-              );
-              copyCapacitorConfigStepSpinner.succeed();
-              copyWebDirStepSpinner.start();
-              fse.copySync(webDirPath, path.join(destDir, "app"));
-              copyWebDirStepSpinner.succeed();
-              npmInstallForPlatformStepSpinner.start();
-              console.log(await runExec(`cd ${destDir} && npm i`));
-              npmInstallForPlatformStepSpinner.succeed();
-              platformSpinner.succeed("Electron platform added!");
-            } else {
-              throw new Error("Electron platform folder already exists.");
-            }
-          } else {
-            throw new Error(
-              "The webDir referenced in capcacitor.config.json does not exsist."
-            );
-          }
+function checkRequirements() {
+  const outPaths = {
+    errorText: null,
+    usersProjectCapConfigPath: null,
+    srcTemplatePath: null,
+    destTemplatePath: null,
+    webAppPath: null,
+  };
+  const usersProjectCapConfig = path.join(
+    process.env.INIT_CWD,
+    "capacitor.config.json"
+  );
+  const srcDir = path.join(__dirname, "../", "electron_template");
+  const destDir = path.join(process.env.INIT_CWD, "electron");
+  if (usersProjectCapConfig) {
+    const capConfigJson = JSON.parse(
+      fs.readFileSync(usersProjectCapConfig, "utf-8")
+    );
+    if (capConfigJson.webDir) {
+      const webDirPath = path.join(process.env.INIT_CWD, capConfigJson.webDir);
+      if (fs.existsSync(webDirPath)) {
+        if (!fs.existsSync(destDir)) {
+          outPaths.destTemplatePath = destDir;
+          outPaths.srcTemplatePath = srcDir;
+          outPaths.usersProjectCapConfigPath = usersProjectCapConfig;
+          outPaths.webAppPath = webDirPath;
         } else {
-          throw new Error(
-            "Property webDir is not defined in capacitor.config.json."
-          );
+          outPaths.errorText = "Electron platform already exists.";
         }
       } else {
-        throw new Error(
-          "No capacitor.config.json file found. Did you initiate capcacitor for this project?"
-        );
+        outPaths.errorText =
+          "WebDir defined in capacitor.config.json does not exist, did you build your webapp?";
       }
     } else {
-      requirementsStepSpinner.info("Dev enviroment, skipping...");
-      platformSpinner.info("Skipped.");
+      outPaths.errorText = "No webDir defined in capacitor.config.json.";
     }
+  } else {
+    outPaths.errorText =
+      "capacitor.config.json does not exist, did you setup capacitor in your project root?";
+  }
+  return outPaths;
+}
+
+function createElectronFolder(srcDir, destDir) {
+  const createElectronFolderStepSpinner = ora({
+    text: "[Electron Platform] Creating electron platform folder...",
+  });
+  createElectronFolderStepSpinner.start();
+  try {
+    fs.mkdirSync(destDir, { recursive: true });
+    fse.copySync(srcDir, destDir);
+    createElectronFolderStepSpinner.succeed();
   } catch (e) {
-    requirementsStepSpinner.fail(`${e.message}`);
+    createElectronFolderStepSpinner.fail();
     throw e;
+  }
+}
+
+function copyRootCapConfig(usersProjectCapConfig, destDir) {
+  const copyCapacitorConfigStepSpinner = ora({
+    text: "[Electron Platform] Copying capacitor.config.json to platform...",
+  });
+  copyCapacitorConfigStepSpinner.start();
+  try {
+    fse.copySync(
+      usersProjectCapConfig,
+      path.join(destDir, "capacitor.config.json")
+    );
+    copyCapacitorConfigStepSpinner.succeed();
+  } catch (e) {
+    copyCapacitorConfigStepSpinner.fail();
+    throw e;
+  }
+}
+
+function copyBuiltWebApp(webDirPath, destDir) {
+  const copyWebDirStepSpinner = ora({
+    text: "[Electron Platform] Copying webDir to platform...",
+  });
+  copyWebDirStepSpinner.start();
+  try {
+    fse.copySync(webDirPath, path.join(destDir, "app"));
+    copyWebDirStepSpinner.succeed();
+  } catch (e) {
+    copyWebDirStepSpinner.fail();
+    throw e;
+  }
+}
+
+async function runNpmInstallOnPlatform(destDir) {
+  const npmInstallForPlatformStepSpinner = ora({
+    text: "[Electron Platform] Running npm install on platform...",
+  });
+  npmInstallForPlatformStepSpinner.start();
+  try {
+    await runExec(`cd ${destDir} && npm i`);
+    npmInstallForPlatformStepSpinner.succeed();
+  } catch (e) {
+    npmInstallForPlatformStepSpinner.fail();
+    throw e;
+  }
+}
+
+async function doPostInstall() {
+  if (!fs.existsSync(path.join(__dirname, "../", ".no-postinstall-script"))) {
+    const requirementsStepSpinner = ora({
+      text: "[Electron Platform] Checking requirements...",
+    });
+    requirementsStepSpinner.start();
+    const paths = checkRequirements();
+    if (paths.errorText === null) {
+      requirementsStepSpinner.succeed();
+      createElectronFolder(paths.srcTemplatePath, paths.destTemplatePath);
+      copyRootCapConfig(
+        paths.usersProjectCapConfigPath,
+        paths.destTemplatePath
+      );
+      copyBuiltWebApp(paths.webAppPath, paths.destTemplatePath);
+      await runNpmInstallOnPlatform(paths.destTemplatePath);
+      console.log("[Electron Platform] Electron platform added!");
+    } else {
+      requirementsStepSpinner.fail();
+      throw new Error(errorText);
+    }
+  } else {
+    console.log(
+      "[Electron Platform] Dev enviroment found, skipping postinstall script."
+    );
   }
 }
 
