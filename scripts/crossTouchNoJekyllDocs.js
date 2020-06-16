317a4b3ae0c7b54e4f5545f13efd92e91a7cfa31
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2020-06-16 19:23:03 -0400

    feat(platform): typescript in template

--- scripts/crossTouchNoJekyllDocs.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-
-try {
-  fs.writeFileSync(path.join(process.env.INIT_CWD, "docs", ".nojekyll"));
-} catch (e) {
-  console.error(e.message);
-}
