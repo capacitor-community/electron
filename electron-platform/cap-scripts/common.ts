82d0839e5336b424b56a2bbb0c5d2ca8e78eaf61
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2020-06-18 14:48:16 -0400

    chore: better cli support and ts scripts

--- electron-platform/cap-scripts/common.ts
+++ electron-platform/cap-scripts/common.ts
@@ -1,22 +1,35 @@
-const { dirname, join, parse, resolve, sep } = require("path");
-const {
-  rmdirSync,
-  readFileSync,
-  existsSync,
-  copyFileSync,
-  writeFileSync,
-  realpathSync,
-  mkdirSync,
-} = require("fs");
+const { dirname, join, parse, resolve } = require("path");
+import { readFileSync, existsSync } from "fs";
+const { exec } = require("child_process");
 const { createHash } = require("crypto");
 const cwd = process.env.INIT_CWD;
 
-//////////////////////////////////////////////////////////////////////
-async function readJSON(path) {
-  const data = readFileSync(path, "utf8");
+export function getCwd(): string | null {
+  // console.log(process.env);
+  const _cwd = process.env.INIT_CWD
+    ? join(process.env.INIT_CWD, "../", "../", "../")
+    : null;
+  return _cwd;
+}
+
+export function readJSON(pathToUse: string): { [key: string]: any } {
+  const data = readFileSync(pathToUse, "utf8");
   return JSON.parse(data);
 }
-function fixName(name) {
+
+export function runExec(command) {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(stdout + stderr);
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+}
+
+export function fixName(name) {
   name = name
     .replace(/\//g, "_")
     .replace(/-/g, "_")
@@ -26,57 +39,14 @@ function fixName(name) {
   return name.charAt(0).toUpperCase() + name.slice(1);
 }
 
-/////////////////////////////////////////////////////////////////////
-async function doUpdate() {
-  const webAppPackageJson = await readJSON(join(cwd, "package.json"));
-  return await runTask("Updating Electron plugins", async () => {
-    const dependencies = webAppPackageJson.dependencies
-      ? webAppPackageJson.dependencies
-      : {};
-    const devDependencies = webAppPackageJson.devDependencies
-      ? webAppPackageJson.devDependencies
-      : {};
-    const deps = Object.keys(dependencies).concat(Object.keys(devDependencies));
-    // get all cap plugins installed
-    let plugins = await Promise.all(deps.map(async (p) => resolvePlugin(p)));
-    // Filter out null returns
-    plugins = plugins.filter((p) => !!p);
-    // Get only the ones with electron "native" plugins
-    let pluginPaths = plugins.map((plugin) => resolveElectronPlugin(plugin));
-    // Filter out nulls
-    pluginPaths = pluginPaths.filter((pluginPath) => !!pluginPath);
-    // Now have list of paths to rollupJs files of electron plugins to use in preload
-    const copyToPath = join(cwd, "electron", "plugins");
-    rmdirSync(copyToPath, { recursive: true });
-    mkdirSync(copyToPath);
-    const filenames = [];
-    for (let i = 0; i < pluginPaths.length; i++) {
-      // console.log(pluginPaths[i] + ' --------');
-      const path = `${pluginPaths[i]}`;
-      let filename = path.substr(path.lastIndexOf(sep) + 1);
-      filename = hashJsFileName(filename, i);
-      copyFileSync(realpathSync(path), join(copyToPath, filename));
-      filenames.push(filename);
-    }
-    let preloaderString = `require('./node_modules/@capacitor-community/electron-core/dist/electron-bridge.js');`;
-    for (const fname of filenames) {
-      preloaderString += `require('./plugins/${fname}');`;
-    }
-    writeFileSync(join(cwd, "electron", "preloader.js"), preloaderString, {
-      encoding: "utf8",
-    });
-
-    //Copy these js files into a path to be used in the preload function.
-    return filenames;
-  });
-}
-function hashJsFileName(filename, slt) {
+export function hashJsFileName(filename, slt) {
   const hash = createHash("md5")
     .update(`${Date.now()}-${slt}-${filename}`)
     .digest("hex");
   return `${filename}-${hash}.js`;
 }
-function resolveNode(...pathSegments) {
+
+export function resolveNode(...pathSegments) {
   const id = pathSegments[0];
   const path = pathSegments.slice(1);
 
@@ -94,7 +64,8 @@ function resolveNode(...pathSegments) {
 
   return join(modulePath, ...path);
 }
-function resolveNodeFrom(start, id) {
+
+export function resolveNodeFrom(start, id) {
   const rootPath = parse(start).root;
   let basePath = resolve(start);
   let modulePath;
@@ -109,7 +80,8 @@ function resolveNodeFrom(start, id) {
     basePath = dirname(basePath);
   }
 }
-async function resolvePlugin(name) {
+
+export async function resolvePlugin(name) {
   try {
     const rootPath = resolveNode(name);
     if (!rootPath) {
@@ -137,7 +109,8 @@ async function resolvePlugin(name) {
   } catch (e) {}
   return null;
 }
-function resolveElectronPlugin(plugin) {
+
+export function resolveElectronPlugin(plugin) {
   if (
     plugin.manifest &&
     plugin.manifest.electron &&
@@ -148,7 +121,8 @@ function resolveElectronPlugin(plugin) {
     return null;
   }
 }
-async function runTask(title, fn) {
+
+export async function runTask(title, fn) {
   const ora = require("ora");
   const spinner = ora(title).start();
   try {
@@ -180,8 +154,3 @@ function formatHrTime(hrtime) {
   }
   return time.toFixed(2) + TIME_UNITS[index];
 }
-
-////////////////////////////////////////////////////////////////////
-(() => {
-  doUpdate();
-})();
