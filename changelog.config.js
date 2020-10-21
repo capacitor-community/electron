cd3f523f98995fb827a011dcc2bd08c92531c90f
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2020-10-21 13:44:19 -0400

    chore: re-add changelog script and config

---
+++ changelog.config.js
@@ -0,0 +1,16 @@
+"use strict";
+const config = require("conventional-changelog-conventionalcommits");
+
+module.exports = config({
+  types: [
+    { type: "feat", section: "Features" },
+    { type: "fix", section: "Bug Fixes" },
+    { type: "chore", section: "Chores" },
+    { type: "docs", section: "Documentation" },
+    { type: "style", hidden: true },
+    { type: "refactor", section: "Refactors" },
+    { type: "perf", hidden: true },
+    { type: "test", hidden: true },
+    { type: "changelog", hidden: true },
+  ],
+});
