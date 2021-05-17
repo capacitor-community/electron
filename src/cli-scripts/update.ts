0f2e3c41fe96566daa18ec5e973b65112451c34b
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2021-05-17 11:35:26 -0400

    chore: clean up update script

--- src/cli-scripts/update.ts
+++ src/cli-scripts/update.ts
@@ -1,24 +1,16 @@
-import { join, sep } from "path";
-import { copyFileSync, writeFileSync, realpathSync, mkdirSync } from "fs";
-import { removeSync } from "fs-extra";
+import { join } from "path";
+import { writeFileSync } from "fs";
 import {
   readJSON,
   resolvePlugin,
   resolveElectronPlugin,
-  hashJsFileName,
-  getCwd,
   runExec
 } from "./common";
 
 
 export async function doUpdate() {
   try {
-    //console.log(process.env.CAPACITOR_ROOT_DIR);
-    //console.log(process.env.CAPACITOR_WEB_DIR);
-    //console.log(process.env.CAPACITOR_CONFIG);
     const usersProjectDir = process.env.CAPACITOR_ROOT_DIR!;
-    //const destDir = join(usersProjectDir, "electron");
-    //const configData = JSON.parse(process.env.CAPACITOR_CONFIG!);
 
     const webAppPackageJson = await readJSON(join(usersProjectDir, "package.json"));
     const dependencies = webAppPackageJson.dependencies
@@ -85,47 +77,4 @@ export async function doUpdate() {
   } catch (e) {
     throw e;
   }
-}
-
-
-
-export async function doUpdate_OLD() {
-  const cwd = getCwd();
-  if (cwd === null) throw new Error("CWD ERROR");
-  const webAppPackageJson = await readJSON(join(cwd, "package.json"));
-  const dependencies = webAppPackageJson.dependencies
-    ? webAppPackageJson.dependencies
-    : {};
-  const devDependencies = webAppPackageJson.devDependencies
-    ? webAppPackageJson.devDependencies
-    : {};
-  const deps = Object.keys(dependencies).concat(Object.keys(devDependencies));
-  // get all cap plugins installed
-  let plugins = await Promise.all(deps.map(async (p) => resolvePlugin(p)));
-  // Filter out null returns
-  plugins = plugins.filter((p) => !!p);
-  // Get only the ones with electron "native" plugins
-  let pluginPaths = plugins.map((plugin) => resolveElectronPlugin(plugin!));
-  // Filter out nulls
-  pluginPaths = pluginPaths.filter((pluginPath) => !!pluginPath);
-  // Now have list of paths to rollupJs files of electron plugins to use in preload
-  const copyToPath = join(cwd, "electron", "plugins");
-  removeSync(copyToPath);
-  mkdirSync(copyToPath);
-  const filenames = [];
-  for (let i = 0; i < pluginPaths.length; i++) {
-    const path = `${pluginPaths[i]}`;
-    let filename = path.substr(path.lastIndexOf(sep) + 1);
-    filename = hashJsFileName(filename, i);
-    copyFileSync(realpathSync(path), join(copyToPath, filename));
-    filenames.push(filename);
-  }
-  let preloaderString = `import '@capacitor/core';`;
-  for (const fname of filenames) {
-    preloaderString += `import './${fname}';`;
-  }
-  writeFileSync(join(cwd, "electron", "preloader.ts"), preloaderString, {
-    encoding: "utf8",
-  });
-  return filenames;
 }
