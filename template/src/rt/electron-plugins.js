1132b6c617e6fb9ee556a62b8a87962c8b4126c5
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2021-06-28 15:33:54 -0400

    chore: template cleanup

--- template/src/rt/electron-plugins.js
+++ template/src/rt/electron-plugins.js
@@ -1,6 +1,2 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const CapacitorDevice = require('C:\\Users\\MichaelSummerfeldt\\Documents\\Projects\\newfolder\\myApp\\electron\\node_modules\\@capacitor\\device\\electron\\dist\\plugin.js')
-
-module.exports = {
-  CapacitorDevice,
-}
+module.exports = {}
