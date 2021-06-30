9aa5623d3944b26f110d0b63622e6ec24dbb8de0
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2021-06-30 12:49:23 -0400

    chore: clean up console logs in rt

--- template/src/rt/electron-rt.ts
+++ template/src/rt/electron-rt.ts
@@ -1,12 +1,9 @@
 import { ipcRenderer, contextBridge } from "electron";
 ////////////////////////////////////////////////////////
 const plugins = require("./electron-plugins");
-console.log(plugins)
 const contextApi: {[plugin: string]: {[functionName: string]: () => Promise<any>}} = {};
 for (const pluginKey of Object.keys(plugins)) {
-  console.log('PluginKey:', pluginKey)
   for (const classKey of Object.keys(plugins[pluginKey])) {
-    console.log('ClassKey:', classKey)
     const functionList = Object.getOwnPropertyNames(
       plugins[pluginKey][classKey].prototype
     ).filter((v) => v !== "constructor");
@@ -14,12 +11,11 @@ for (const pluginKey of Object.keys(plugins)) {
       contextApi[classKey] = {};
     }
     for (const functionName of functionList) {
-      console.log('FunctionKey:', functionName)
       if (!contextApi[classKey][functionName]) {
         contextApi[classKey][functionName] = () => ipcRenderer.invoke(`${classKey}-${functionName}`);
       }
     }
   }
 }
-contextBridge.exposeInMainWorld('CapacitorCustomPlatform', {name: 'electron', useWebFallbackPlugins: true, plugins: contextApi})
+contextBridge.exposeInMainWorld('CapacitorCustomPlatform', {name: 'electron', plugins: contextApi})
 ////////////////////////////////////////////////////////
