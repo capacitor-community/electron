c303772b62ff8d9e6795606c282fb36b1cca3695
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2021-07-23 15:12:08 -0400

    chore: cleanup

---
+++ packages/docs/build/assets/js/814f3328.7436c4e9.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[535],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/blog/welcome"},{"title":"Hello","permalink":"/blog/hello-world"},{"title":"Hola","permalink":"/blog/hola"}]}')}}]);
---
+++ packages/docs/build/assets/js/e16015ca.66eefc30.js
@@ -0,0 +1,1 @@
+(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[700],{947:function(a){"use strict";a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"hola","name":"hola","count":1,"permalink":"/blog/tags/hola"}')}}]);
