b0df0172acef4f4432a1d21d7c9038db8b0cca86
Mike Summerfeldt <20338451+IT-MikeS@users.noreply.github.com>
2023-05-12 02:48:30 -0400

    feat: new repo structure

--- docs/assets/js/6ef57d7a.3fdfe3f9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[341],{3838:function(s){s.exports=JSON.parse('{"allTagsPath":"/electron/blog/tags","slug":"/electron/blog/tags/docs","name":"docs","count":1,"permalink":"/electron/blog/tags/docs"}')}}]);
--- platform/.prettierrc.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-  ...require('@ionic/prettier-config'),
-};
