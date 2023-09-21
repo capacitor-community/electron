acf89829cb029c029ddc8fe545c58842ad55f188
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2023-09-21 10:12:06 -0400

    chore: package update

--- src/electron-platform/util.ts
+++ src/electron-platform/util.ts
@@ -149,9 +149,7 @@ export async function encodeFromFile(filePath: string): Promise<string> {
   }
   const fileData = readFileSync(filePath);
   mediaType = /\//.test(mediaType) ? mediaType : 'image/' + mediaType;
-  const dataBase64 = Buffer.isBuffer(fileData)
-    ? fileData.toString('base64')
-    : Buffer.from(fileData).toString('base64');
+  const dataBase64 = Buffer.isBuffer(fileData) ? fileData.toString('base64') : Buffer.from(fileData).toString('base64');
   return 'data:' + mediaType + ';base64,' + dataBase64;
 }
 
